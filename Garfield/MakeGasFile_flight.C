3045ff9f7da94e371e6cd1e97ba4c7ff419af941
keithwm422 <keithwm.422@gmail.com>
2025-01-12 12:42:04 -0600

    Isochron generator not finalized but example here. Makefile and MakeGasfile_flight updated for proper Efield regions

--- Garfield/MakeGasFile_flight.C
+++ Garfield/MakeGasFile_flight.C
@@ -25,34 +25,38 @@ int main(int argc, char * argv[]) {
   const double temperature = 273.15 + invals[1];
   std::cout << "temperature in kelvin: " << temperature << std::endl;
   std::stringstream outfilename;
-  //outfilename << "Flight2024_Boff_P_" << pressure <<"_T_" << temperature << "_.gas";      
-  outfilename << "Flight2024_Bon_P_" << pressure <<"_T_" << temperature << "_.gas";      
+  //outfilename << "Flight2024_Boff_P_" << pressure <<"_T_" << temperature << "_.gas";
+  outfilename << "Flight2024_Boff_P_" << pressure <<"_T_" << temperature << "_multiE_90CO2_10Ar_01122024.gas";
 
   // Setup the gas.
   MediumMagboltz* gas = new MediumMagboltz();
   gas->SetTemperature(temperature);
   gas->SetPressure(pressure);
   gas->SetComposition("CO2", 90.,"AR", 10.);
- 
+
   // Set the field range to be covered by the gas table. 
-  const int nFields = 1;
-  const double E_not = 984.25;
-  const double emin = E_not;
-  const double emax = E_not;
-  //for efield drift study
-  //const int nFields = 20;
-  //make E_not the midpt
+  //const int nFields = 1;
   //const double E_not = 984.25;
-  //const double emin = E_not-200.0;
-  //const double emax = E_not+200.0;
+  //const double emin = E_not;
+  //const double emax = E_not;
+  //for efield drift study
+  const int nFields = 11;
+  //make E_not the midpt
+  const double E_not = 984.25;
+  const double emin = E_not-E_not;
+  const double emax = E_not+E_not;
 
   // Flag to request logarithmic spacing.
   const bool useLog = false;
-  const double bmin=1;
-  const double bmax=1; // do we need magnetic field on?
+  const double bmin=0;
+  const double bmax=0; // do we need magnetic field on?
   const int nBFields=1;
   gas->SetFieldGrid(emin, emax, nFields, useLog, bmin,bmax,nBFields,TMath::Pi()/2.0,TMath::Pi()/2.0,1); 
 
+  // Turn on penning transfer?
+  gas->EnablePenningTransfer();
+  gas->SetMaxElectronEnergy(200);
+  std::cout << "number of levels: " << gas->GetNumberOfLevels();
   const int ncoll = 10;
   // Switch on debugging to print the Magboltz output.
   gas->EnableDebugging();
